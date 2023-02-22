aff188aa58193e4c038423a633453390bfd1ba21
Jack Fletcher <jackowenfletcher@gmail.com>
2023-02-22 22:07:08 +0000

    Add vite build test

---
+++ test/loader.test.ts
@@ -0,0 +1,23 @@
+import { assert, describe, expect, it } from "vitest";
+
+import { ViteCooklangRecipeLoaderPlugin } from "../src/index";
+import { build } from "vite";
+import path from "path";
+
+const EXAMPLE_PROJECT_DIR = path.resolve(__dirname, "./example");
+
+describe("integration", () => {
+  it("builds", async () => {
+    await build({
+      root: EXAMPLE_PROJECT_DIR,
+      build: {
+        rollupOptions: {
+          // ...
+          plugins: [ViteCooklangRecipeLoaderPlugin()],
+        },
+      },
+    });
+
+    expect(1 + 1).eq(2);
+  });
+});
